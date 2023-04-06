842701268d8278fbf82cb481862ae92fb4c25c2e
shawn615 <62926141+shawn615@users.noreply.github.com>
2023-04-06 23:36:39 +0900

    Update eval.py

--- eval.py
+++ eval.py
@@ -13,7 +13,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Evaluation script for RegNeRF."""
+"""Evaluation script for MixNeRF."""
 import functools
 from os import path
 import time
